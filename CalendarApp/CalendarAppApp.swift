dda1af04b5c6d01c5d1067d7665c6cd835a362cb
Michelle Stafford <elle.stafford@gmail.com>
2022-08-19 20:41:36 -0700

    Initial Commit

---
+++ CalendarApp/CalendarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAppApp.swift
+//  CalendarApp
+//
+//  Created by Michelle Stafford on 2022-08-19.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
